e51e8891b904dca4bffb2b699f602d1624a2fdef
caoke123 <jayxp52194980@gmail.com>
2026-06-04 12:31:16 +0800

    fix: add open-next config for cloudflare build

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_R2_BASE_URL":"https://yutu.nv315.top","NEXT_PUBLIC_CATALOG_PATH":"/catalogs","PORT":"3010"};
+export const development = {"NEXT_PUBLIC_R2_BASE_URL":"https://yutu.nv315.top","NEXT_PUBLIC_CATALOG_PATH":"/catalogs","PORT":"3010"};
+export const test = {};
